93eb056f6f88f7667dac056fdf66242078f94b30
Jason Wang <91506829+Leek727@users.noreply.github.com>
2021-10-03 16:01:19 -0400

    Add files via upload
    Added file to have a random chance of showing olof

---
+++ olof_timer.py
@@ -0,0 +1,11 @@
+import time
+import random
+from subprocess import Popen, PIPE
+
+
+olof_perc = int(input("Olof percent per second : "))
+while True:
+    time.sleep(1)
+    if random.random() < (olof_perc / 100):
+        process = Popen(['python3', 'olofcum.py'], stdout=PIPE, stderr=PIPE)
+        stdout, stderr = process.communicate()
