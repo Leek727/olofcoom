8c4e8e49c4e2d1bdb7723996466e61c4d1ec9047
Jason Wang <91506829+Leek727@users.noreply.github.com>
2021-10-03 16:12:05 -0400

    Added sounds clip to olof

--- olof_timer.py
+++ olof_timer.py
@@ -1,7 +1,11 @@
 import time
 import random
 from subprocess import Popen, PIPE
+from pygame import mixer  # Load the popular external library
+import pygame
 
+mixer.init()
+mixer.music.load('olofyaoi.ogg')
 
 olof_perc = float(input("Olof percent per second : "))
 while True:
@@ -9,3 +13,8 @@ while True:
     if random.random() < (olof_perc / 100):
         process = Popen(['python3', 'olofcum.py'], stdout=PIPE, stderr=PIPE)
         stdout, stderr = process.communicate()
+
+        mixer.music.play()
+
+        while pygame.mixer.music.get_busy(): 
+            pygame.time.Clock().tick(10)
