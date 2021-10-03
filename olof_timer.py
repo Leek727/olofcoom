9771c023dee15de43802f966c55c21d829e1856e
Jason Wang <91506829+Leek727@users.noreply.github.com>
2021-10-03 16:29:14 -0400

    changed resrouce path

--- olof_timer.py
+++ olof_timer.py
@@ -5,7 +5,7 @@ from pygame import mixer  # Load the popular external library
 import pygame
 
 mixer.init()
-mixer.music.load('olofyaoi.ogg')
+mixer.music.load('resources/olofyaoi.ogg')
 
 olof_perc = float(input("Olof percent per second : "))
 while True:
