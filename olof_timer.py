7dcc4217bf15be6a652041c5144f32042f8cf0be
Jason Wang <91506829+Leek727@users.noreply.github.com>
2021-10-03 17:34:48 -0400

    renamed to olofjupscare and fixed bg transparency

--- olof_timer.py
+++ olof_timer.py
@@ -11,7 +11,7 @@ olof_perc = float(input("Olof percent per second : "))
 while True:
     time.sleep(1)
     if random.random() < (olof_perc / 100):
-        process = Popen(['python3', 'olofcum.py'], stdout=PIPE, stderr=PIPE)
+        process = Popen(['python3', 'olofjumpscare.py'], stdout=PIPE, stderr=PIPE)
         stdout, stderr = process.communicate()
 
         mixer.music.play()
---
+++ olofjumpscare.py
@@ -0,0 +1,70 @@
+from PyQt5.QtWidgets import *
+from PyQt5.QtGui import QPixmap
+import sys
+ 
+from PyQt5 import QtGui, QtCore, uic
+from PyQt5 import QtWidgets
+from PyQt5.QtWidgets import QMainWindow, QApplication
+ 
+import sys
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import *
+
+class Window(QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        self.setWindowFlags(
+            QtCore.Qt.WindowStaysOnTopHint |
+            QtCore.Qt.FramelessWindowHint |
+            QtCore.Qt.X11BypassWindowManagerHint
+        )
+        self.setGeometry(
+            QtWidgets.QStyle.alignedRect(
+                QtCore.Qt.LeftToRight, QtCore.Qt.AlignCenter,
+                QtCore.QSize(220, 32),
+                QtWidgets.qApp.desktop().availableGeometry()
+        ))
+        self.setAttribute(QtCore.Qt.WA_TranslucentBackground)
+ 
+        self.acceptDrops()
+        # set the title
+        self.setWindowTitle("Image")
+ 
+        # setting  the geometry of window
+        self.setGeometry(0, 0, 1920, 1080)
+
+        self.setWindowOpacity(0.7)
+ 
+        # creating label
+        self.label = QLabel(self)
+         
+        # loading image
+        self.pixmap = QPixmap('resources/bigolof.png')
+ 
+        # adding image to label
+        self.label.setPixmap(self.pixmap)
+
+        # Optional, resize label to image size
+        self.label.resize(self.pixmap.width(),
+                          self.pixmap.height())
+ 
+
+        # show all the widgets
+        self.show()
+
+
+    def mousePressEvent(self, event):
+        QtWidgets.qApp.quit()
+        
+    
+ 
+# create pyqt5 app
+App = QApplication(sys.argv)
+ 
+# create the instance of our Window
+window = Window()
+
+# start the app
+exit(App.exec())
