2a9c6386427a5ea5d5f1458eea83e17d234db26d
Jason Wang <91506829+Leek727@users.noreply.github.com>
2021-10-04 19:00:46 -0400

    Made motivational olof appear on start
    Also removed unused stuff

--- olof_timer.py
+++ olof_timer.py
@@ -7,12 +7,13 @@ import pygame
 mixer.init()
 mixer.music.load('resources/olofyaoi.ogg')
 
+process = Popen(['python3', 'motivationalolof.py'], stdout=PIPE, stderr=PIPE)
+
 olof_perc = float(input("Olof percent per second : "))
 while True:
     time.sleep(1)
     if random.random() < (olof_perc / 100):
         process = Popen(['python3', 'olofjumpscare.py'], stdout=PIPE, stderr=PIPE)
-        stdout, stderr = process.communicate()
 
         mixer.music.play()
 
